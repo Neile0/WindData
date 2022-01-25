99bd664b4f3c0e8d5ad6e0dafc62a08e54dfaa73
Aidan Neil <29099867+Neile0@users.noreply.github.com>
2022-01-25 23:58:51 +0000

    Closes 1. Imports data from selected file.

--- WindData/MainWindow.xaml.cs
+++ WindData/MainWindow.xaml.cs
@@ -1,17 +1,8 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace WindData
 {
@@ -20,9 +11,51 @@ namespace WindData
     /// </summary>
     public partial class MainWindow : Window
     {
+        private string ImportedFileName;
+        private Dictionary<string, string> ImportedData = new Dictionary<string, string>();
+        private float Displacement;
+        private float AwL; // Longitudinal Windage Area
+        private float AwT; // Transverse Windage Area
+        private float zL; // 
+        private float zT;
+        private int N;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Print()
+        {
+            txtDisplay.Text = String.Format("Filepath: {0}\n\nDisplacement: {1}\nAwL: {2}\nAwT:{3}\nzL: {4}\nzT: {5}\nN: {6}", ImportedFileName, Displacement, AwL, AwT, zL, zT, N);
+        }
+
+        private void ParseResultsFile(String filePath)
+        {
+            foreach (string line in File.ReadAllLines(filePath))
+            {
+                var keyValue = line.Split(':');
+                ImportedData.Add(keyValue[0], keyValue[1].Trim());
+            }
+
+            Displacement = float.Parse(ImportedData["Displacement"]);
+            AwL = float.Parse(ImportedData["AwL"]);
+            AwT = float.Parse(ImportedData["AwT"]);
+            zL = float.Parse(ImportedData["zL"]);
+            zT = float.Parse(ImportedData["zT"]);
+            N = int.Parse(ImportedData["N"]);
+        }
+
+        private void btnFileOpen_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+            {
+                ImportedFileName = openFileDialog.FileName;
+            }
+            ParseResultsFile(ImportedFileName);
+            txtFileLocation.Text = ImportedFileName;
+            Print();
         }
     }
 }
